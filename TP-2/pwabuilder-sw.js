14a1a0e937fb7ef5581c9585a7d0ff44bba2a629
Matias24120 <137196771+Matias24120@users.noreply.github.com>
2023-06-30 22:56:39 -0300

    Add files via upload

--- TP-2/pwabuilder-sw.js
+++ TP-2/pwabuilder-sw.js
@@ -10,6 +10,33 @@ self.addEventListener("message", (event) => {
   }
 });
 
+self.addEventListener(`install`,()=>{
+	//Guardo los archivos en el cache
+	const cache = caches.open('mi-cache-1').then( cache => {
+	// Guarda los datos del caché necesario para que la app funciones sin conexión
+	return cache.addAll(['/','index.html', 'css/style.css' , 'detalle.html' ,'js/main.js','js/detalle.js', 'js/bootstrap.bundle.min.js', 'img/favoritos.png' ]);
+})
+// Espera hasta que la promesa se resuelva
+event.waitUntil( cache)
+})
+
+self.addEventListener(`fetch`, (event) => {
+  // Aplicar estrategia del caché si falla la web
+  const respuestaCache = caches.match(event.request).then((res) => {
+    if (res) {
+      return res;
+    } else {
+      // Si no, hacemos un fetch
+      return fetch(event.request).then((respuesta) => {
+        return respuesta;
+      });
+    }
+  });
+
+  event.respondWith(respuestaCache);
+});
+
+
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
